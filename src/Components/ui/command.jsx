39a0e95276ca4a4831ccd1f7ad5e827d5a90690f
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-12-02 12:47:10 +0100

    removed unused import

--- src/Components/ui/command.jsx
+++ src/Components/ui/command.jsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import {Dialog, DialogContent, DialogProps} from "@radix-ui/react-dialog"
+import {Dialog, DialogContent} from "@radix-ui/react-dialog"
 import { Command as CommandPrimitive } from "cmdk"
 import { Search } from "lucide-react"
 
