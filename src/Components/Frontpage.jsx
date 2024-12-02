ee23480b56e83950c2e9fb04cc5eb403d4f88c49
hantamil <mira0004@stud.kea.dk>
2024-12-02 10:11:30 +0100

    small changes

--- src/Components/Frontpage.jsx
+++ src/Components/Frontpage.jsx
@@ -10,7 +10,14 @@ import {
     NavigationMenuTrigger,
     NavigationMenuViewport,
 } from "@radix-ui/react-navigation-menu"
-import Gif from "@/Components/ui/react-gif.jsx";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 
 export default function Frontpage() {
@@ -33,7 +40,7 @@ export default function Frontpage() {
                             og her er endnu mere tekst for at teste scroll, nu hpber jeg sku det virker pls altså hvad fuck er der galt</p>
                     </ScrollArea>
                     <div className="flex-col w-2/3 mt-8">
-                        <ScrollArea className="h-[30vh] rounded-xl p-4 overflow-y-auto scrollbar-thin scrollbar-custom bg-[#48B936] bg-opacity-60">
+                        <ScrollArea className="h-[30vh] rounded-xl p-4 overflow-y-auto scrollbar-thin scrollbar-custom bg-[#48B936] bg-opacity-20">
                         <h1 className="text-4xl text-center p-4 font-bold text-[#003811]">Nuværende kampagner</h1>
                         <p className="text-2xl p-4 text-[#003811]">Sidste uge havde vi fornøjelsen af at invitere en gruppe af vores optometristuderende med til fabrikken i Ungarn 🤓
 
@@ -48,42 +55,85 @@ export default function Frontpage() {
                     </ScrollArea>
 
                         <div className="justify-between mt-8">
-                            <NavigationMenu>
-                                <NavigationMenuList className="flex gap-8">
-                                    <NavigationMenuItem className="group relative">
-                                        <NavigationMenuTrigger className="hover:bg-[#003811] h-[5vh] w-[30vh] px-4 py-2 bg-[#00693c] text-white rounded-md font-bold text-2xl">BRILLER</NavigationMenuTrigger>
-                                        <NavigationMenuContent className="flex flex-col text-2xl text-[#003811] text-center">
-                                            <NavigationMenuLink><a href="https://media.relesysapp.net/secure/media-secure/910/4415a5e8-33a1-4465-a70d-c230ed9b4a10/Louis_Nielsen_Produktkatalog_-_efteraar_2024.pdf"
-                                                                   target="_blank"
-                                                                   rel="noopener noreferrer" className="hover:font-bold">Glas katalog</a></NavigationMenuLink>
-                                            <NavigationMenuLink><a href="https://media.relesysapp.net/secure/media-secure/910/cee356a1-2f67-42d5-bd94-1e65bb4da63e/Produktkatalog_-_overblik_over_glas.pdf"
-                                                                   target="_blank"
-                                                                   rel="noopener noreferrer" className="hover:font-bold">Lens Overview - Varifocals</a></NavigationMenuLink>
-                                            <NavigationMenuLink><a href="https://media.relesysapp.net/secure/media-secure/910/d21f2eb0-59a2-478d-b263-e8569b9073c7/Optileks_katalog_NESO_version_-_ny_sept_24.pdf"
-                                                                   target="_blank"
-                                                                   rel="noopener noreferrer" className="hover:font-bold">Optileks katalog</a></NavigationMenuLink>
-                                            <NavigationMenuLink><a href="https://media.relesysapp.net/secure/media-secure/910/15e5488a-5e8e-48ee-9619-cda9a6f5cfef/Specs2_Extra_Pricelist_DK_nov2024_-_ANL.pdf"
-                                                                   target="_blank"
-                                                                   rel="noopener noreferrer" className="hover:font-bold">Optileks prisliste</a></NavigationMenuLink>
-                                        </NavigationMenuContent>
-                                    </NavigationMenuItem>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger className="hover:bg-[#003811] h-[5vh] w-[30vh] px-4 py-2 bg-[#00693c] text-white rounded-md font-bold text-2xl">
+                                    BRILLER
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent className="p-2 shadow-none border-none"
+                                                     style={{ backgroundColor: "transparent" }}>
+                                    <DropdownMenuItem asChild>
+                                        <a
+                                            href="https://media.relesysapp.net/secure/media-secure/910/4415a5e8-33a1-4465-a70d-c230ed9b4a10/Louis_Nielsen_Produktkatalog_-_efteraar_2024.pdf"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="hover:font-bold text-[#003811] hover:bg-transparent"
+                                        >
+                                            Glas katalog
+                                        </a>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem asChild>
+                                        <a
+                                            href="https://media.relesysapp.net/secure/media-secure/910/cee356a1-2f67-42d5-bd94-1e65bb4da63e/Produktkatalog_-_overblik_over_glas.pdf"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="hover:font-bold text-[#003811] hover:bg-transparent"
+                                        >
+                                            Lens Overview - Varifocals
+                                        </a>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem asChild>
+                                        <a
+                                            href="https://media.relesysapp.net/secure/media-secure/910/d21f2eb0-59a2-478d-b263-e8569b9073c7/Optileks_katalog_NESO_version_-_ny_sept_24.pdf"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="hover:font-bold text-[#003811] hover:bg-transparent"
+                                        >
+                                            Optileks katalog
+                                        </a>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem asChild>
+                                        <a
+                                            href="https://media.relesysapp.net/secure/media-secure/910/15e5488a-5e8e-48ee-9619-cda9a6f5cfef/Specs2_Extra_Pricelist_DK_nov2024_-_ANL.pdf"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="hover:font-bold text-[#003811] hover:bg-transparent"
+                                        >
+                                            Optileks prisliste
+                                        </a>
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
 
-                                    <NavigationMenuItem>
-                                        <NavigationMenuTrigger className="hover:bg-[#003811] h-[5vh] w-[30vh] px-4 py-2 bg-[#00693c] text-white rounded-md font-bold text-2xl">KONTAKTLINSER</NavigationMenuTrigger>
-                                        <NavigationMenuContent className="flex flex-col text-2xl text-[#003811] text-center">
-                                            <NavigationMenuLink><a href="https://media.relesysapp.net/secure/media-secure/910/99bbc181-1540-4897-8e34-59d4db7fe1be/Prisliste_maj_24.pdf"
-                                                                   target="_blank"
-                                                                   rel="noopener noreferrer" className="hover:font-bold">Prisliste</a></NavigationMenuLink>
-                                            <NavigationMenuLink><a href="https://media.relesysapp.net/secure/media-secure/910/d030adcf-bf07-4a43-9b60-e5e3425661b9/Prisliste_core.pdf"
-                                                                   target="_blank"
-                                                                   rel="noopener noreferrer" className="hover:font-bold">Core Range tabel</a></NavigationMenuLink>
-                                        </NavigationMenuContent>
-                                    </NavigationMenuItem>
-                                </NavigationMenuList>
-                            </NavigationMenu>
-                            <div className="w-full h-full flex justify-center items-center object-cover">
-                                <Gif src="/briller.gif"/> {/* det burde ikke være gif */}
-                            </div>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger className="hover:bg-[#003811] h-[5vh] w-[30vh] px-4 py-2 bg-[#00693c] text-white rounded-md font-bold text-2xl mt-4">
+                                    KONTAKTLINSER
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent  className="p-2 shadow-none border-none"
+                                                      style={{ backgroundColor: "transparent" }}>
+                                    <DropdownMenuItem asChild>
+                                        <a
+                                            href="https://media.relesysapp.net/secure/media-secure/910/99bbc181-1540-4897-8e34-59d4db7fe1be/Prisliste_maj_24.pdf"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="hover:font-bold text-[#003811] hover:bg-transparent"
+                                        >
+                                            Prisliste
+                                        </a>
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem asChild>
+                                        <a
+                                            href="https://media.relesysapp.net/secure/media-secure/910/d030adcf-bf07-4a43-9b60-e5e3425661b9/Prisliste_core.pdf"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="hover:font-bold text-[#003811] hover:bg-transparent"
+                                        >
+                                            Core Range tabel
+                                        </a>
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+
+                            <img src="/brille.png" alt="brille"/> {/*jeg kan ikke få billedet til at vise sig*/}
                         </div>
                      </div>
                 </div>
