a40fae65858367de646e5d6f981448a224a80c40
LasseThomsen <lasse.thomsen@hotmail.dk>
2024-12-05 10:36:05 +0100

    searchbar

--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -1,22 +1,25 @@
 import {Input} from "@/Components/ui/input.jsx";
 import {Button} from "@/Components/ui/button.jsx";
-import {useState} from "react";
 
-export default function SearchBar({handleSearch}) {
+export default function SearchBar({handleSearch, handleChange}) {
+    /* det her er hvordan de forskellige states og funktioner ville se ud hvis de var i dette komponent
     const [search, setSearch] = useState("")
 
-    const handleSubmit = (e) => {
+    const handleSearch = (e) => {
         e.preventDefault();
         console.log(search)
         setSearch("") //reset search
         //redirect to search result page
     }
-
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+    }
+    */
 
 
     return (
         <form className={"flex "} onSubmit={handleSearch}>
-            <Input className={"mr-2 text-gray-700"} type={"text"} onChange={(e) => setSearch(e.target.value)} placeholder="Søg efter nyheder" />
+            <Input className={"mr-2 text-gray-700"} type={"text"} onChange={handleChange} placeholder="Søg efter nyheder" />
             <Button type={"submit"}>Søg</Button>
         </form>
 
